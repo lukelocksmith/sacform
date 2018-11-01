516ea6f6a29207b436322b25563391d095ea25bb
Luke Locksmith <lukaslesarev@gmail.com>
2018-11-01 16:04:36 +0100

    add home screen

--- src/js/_main.js
+++ src/js/_main.js
@@ -1,14 +1,14 @@
-$(".social").click(function(){
-    $(".social-list").toggle();
-});
-$(".language").click(function(){
-    $(".lang-list").toggle();
-});
-$( ".lang-en" ).click(function() {
-  $( ".en-text" ).show();
-  $( ".pl-text" ).hide();
-});
-$( ".lang-pl" ).click(function() {
-  $( ".pl-text" ).show();
-  $( ".en-text" ).hide();
-});
+// $(".social").click(function(){
+//     $(".social-list").toggle();
+// });
+// $(".language").click(function(){
+//     $(".lang-list").toggle();
+// });
+// $( ".lang-en" ).click(function() {
+//   $( ".en-text" ).show();
+//   $( ".pl-text" ).hide();
+// });
+// $( ".lang-pl" ).click(function() {
+//   $( ".pl-text" ).show();
+//   $( ".en-text" ).hide();
+// });
